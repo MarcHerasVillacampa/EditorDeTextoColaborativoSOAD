1b5e9bc7d300c506742d2378b4a0049709394d1a
MarcHerasVillacampa <106541914+MarcHerasVillacampa@users.noreply.github.com>
2022-05-30 17:41:59 +0200

    Update editTxt.py

--- editTxt.py
+++ editTxt.py
@@ -10,13 +10,6 @@ def insertInTxt(file,position,text):
         f.write(content[position:])
         f.close()
 
-# def insertInTxt(file,position,text):
-#     print("Meter en el TXT esto ------------->",text)
-#     with open(file, 'a') as f:
-#         f.write(text)
-#         f.close()
-
-
 def ereaseInTxt(file,position,amount):
     print("Borrar en el TXT esto ------------->",position," ",amount)
     with open(file, 'r+') as f:
@@ -26,8 +19,3 @@ def ereaseInTxt(file,position,amount):
         f.write(text[int(position+1)+int(amount):])
         f.flush()
         f.close()
-
-# ereaseInTxt("foo.txt",2,2)
-
-# while True:
-#     insertInTxt("foo.txt",1,input())
